9a37b6368e6b52cc30ddc9d91ec7485848d8ca7b
Wongsathorn Panichkurkul <wongsathorn.pa@ku.th>
2018-11-12 21:35:53 +0700

    Fixed: bugs in Util.java

--- src/Utils.java
+++ src/Utils.java
@@ -4,47 +4,42 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Utils {
 
 	public static void main(String[] args) throws IOException {
+
+		AtomicInteger counter = new AtomicInteger(0);
 		FileWriter fileWriter = new FileWriter(new File("./DependencyData.csv"));
 		PrintWriter printWriter = new PrintWriter(fileWriter);
-		printWriter.print("Instability, Abstractness\n");
-		List<Double> ins = new ArrayList<>();
-		List<Double> abs = new ArrayList<>();
+		printWriter.print("Instability,Abstractness\n");
 
 		try {
-
-			AtomicInteger numFile = new AtomicInteger(0);
-			FileRead fr = new FileRead();
-			Files.walk(Paths.get("/Users/JamieSK/Downloads/axis2-1.7.8/")).forEach(file -> {
+			Files.walk(Paths.get("C:\\Users\\Mond\\workspace\\axis2-1.7.8")).forEach(file -> {
 				File dir = new File(file.toString());
 				if (dir.isDirectory()) {
+					FileRead fr = new FileRead();
 					for (String myFile : dir.list()) {
 						if (myFile.contains(".java")) {
 							fr.readInFile(dir.toString(), myFile);
-							numFile.incrementAndGet();
 						}
 					}
-					if (fr.getNa() != 0 && fr.getNc() != 0 && fr.getCa() != 0 && fr.getCe() != 0) {
-						PackageInfo pack = new PackageInfo(fr.getNa(), fr.getNc(), fr.getCa(), fr.getCe());
-						ins.add(pack.getInstability());
-						abs.add(pack.getAbstractness());
-						printWriter.printf("%.6f, %.6f \n", pack.getInstability(), pack.getAbstractness());
-					}
+					System.out.println(counter.incrementAndGet());
+					System.out.println(dir);
+
+					PackageInfo pack = new PackageInfo(fr.getNa(), fr.getNc(), fr.getCa(), fr.getCe());
+
+					if (!Double.isNaN(pack.getInstability()) && !Double.isNaN(pack.getAbstractness()))
+						printWriter.printf("%.6f,%.6f \n", pack.getInstability(), pack.getAbstractness());
+
 				}
 			});
-			System.out.println(numFile.get());
-			for (int i = 0; i < ins.size() - 1; i++) {
-				System.out.printf("%.6f, %.6f \n", ins.get(i), abs.get(i));
-			}
+			System.out.println(counter.get() + " files has been read");
 			printWriter.close();
 		} catch (Exception e) {
 			return;
 		}
 	}
+
 }
